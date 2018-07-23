068b4311b04b7a1f411e7e9429d71444d2bf14ab
Jordan <lozzadawg65@gmail.com>
2018-07-24 07:32:38 +1000

    work on movement
    about halfway through the keys being able to control the selected block

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ canvas.height=600;
 var c = canvas.getContext("2d");
 
 
-var column1 = []
-var column2 = []
-var column3 = []
+
 
 var size = 30;
 //Double xPos
@@ -50,13 +48,17 @@ archBlock.canRender=true;
 // const gamesBlock12 = new Block(canvas.width/(size/10),canvas.height/(size/5),(canvas.width/size)*3,(canvas.height/size)*2,"black");
 // const gamesBlock13 = new Block(canvas.width/(size/10),canvas.height/(size/8),(canvas.width/size)*3,(canvas.height/size)*2,"black");
 
-var selectedBlock = gamesBlock;
+var column1 = [aboutMeBlock,gamesBlock,archBlock]
+var column2 = []
+var column3 = []
+var allColumns = [column1,column2,column3];
 
-
-column1.push(aboutMeBlock,gamesBlock,archBlock);
 // for(var i = 0; i < selectedBlock.sublist.length;i++){
 //   column2.push(selectedBlock.sublist[i]);
 // }
+var currentColumn = 0;
+var currentRow = 1;
+var selectedBlock = allColumns[currentColumn][currentRow];
 
 //get 1st column movement, then start to render 2nd column, then get cross coulmn movement
 
